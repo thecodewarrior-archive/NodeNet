64208a9af01a588b9343605042ef919804001731
Pierce Corcoran <pierce@plasticcow.com>
2018-10-20 01:38:40 -0700

    Improve redstone power condition

--- src/main/java/com/thecodewarrior/nodenet/common/nodes/RedstoneReaderNode.kt
+++ src/main/java/com/thecodewarrior/nodenet/common/nodes/RedstoneReaderNode.kt
@@ -3,13 +3,15 @@ package com.thecodewarrior.nodenet.common.nodes
 import com.thecodewarrior.nodenet.common.entity.EntityNode
 import com.thecodewarrior.nodenet.common.node.Node
 import com.thecodewarrior.nodenet.common.node.Signal
+import net.minecraft.init.Blocks
+import net.minecraft.util.EnumFacing
 import net.minecraft.util.math.BlockPos
 import kotlin.math.floor
 
 class RedstoneReaderNode(entity: EntityNode): Node(entity) {
     override fun computeSignal(): Signal {
         val pos = BlockPos(floor(entity.posX), floor(entity.posY), floor(entity.posZ))
-        return RedstoneSignal(entity.world.isBlockPowered(pos))
+        return RedstoneSignal(EnumFacing.values().any { entity.world.getRedstonePower(pos, it) > 0 })
     }
 }
 
